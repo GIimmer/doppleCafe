c8c6751eb4c703b8c612fcda9c163d4a6bb20820
Riley Waugh <v-riwaug@microsoft.com>
2020-03-30 19:09:36 -0700

    K-Means running

--- cafeTest.py
+++ cafeTest.py
@@ -1,6 +1,7 @@
 import requests
 import functools
 import json
+import copy
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -94,22 +95,40 @@ def processAreaSearch(rawCafeArr):
 
 def processCafeArray(cafeArr):
     cafesToStrip = []
+    reviewVectors = []
     for cafe in cafeArr:
         res = vectorizeCafeReviews(cafe)
-        if res == -1:
+        if len(res) > 0:
+            reviewVectors.append(res)
+        else:
             cafesToStrip.append(cafe)
+
     for cafe in cafesToStrip:
         cafeArr.remove(cafe)
 
+    tfIDFVectors = processReviews(reviewVectors)
+    return tfIDFVectors
+
+def processReviews(reviewVectors):
+    ReviewVectors = np.matrix(reviewVectors)
+    numReviews = ReviewVectors.shape[0]
+    logicalVectors = ReviewVectors != 0
+    docFreq = logicalVectors.sum(axis=0)
+
+    def calculateIDF(x):
+        return np.log(numReviews/(x + 1)) + 1
+    calculateIDF = np.vectorize(calculateIDF)
+    idf = calculateIDF(docFreq)
+    return np.multiply(ReviewVectors, idf)
 
 def vectorizeCafeReviews(cafeObj):
     reviewText = combineReviewText(cafeObj['reviews'])
     if len(reviewText) == 0:
-        return -1
+        return []
     vectorizedText = convertTextToTFVector(reviewText)
     cafeObj['reviewVect'] = vectorizedText
 
-    return 1
+    return vectorizedText
 
 def combineReviewText(reviewArr):
     finalText = ''
@@ -127,12 +146,9 @@ def convertTextToTFVector(text):
 
     vect = np.zeros(wordBagLen)
     for key, val in freqDict.items():
-        try:
-            idxOfKeyInVector = vectorIdxRef[key]
-            vect[idxOfKeyInVector] = (val/cafeReviewsLen)
-        except:
-            print(key)
-            
+        idx = vectorIdxRef.get(key, -1)
+        if idx != -1:
+            vect[idx] = (val/cafeReviewsLen)     
     return vect
 
 def stemAndRemoveStopwordsFromStringArr(stringArr):
@@ -149,11 +165,54 @@ def stemAndRemoveStopwordsFromStringArr(stringArr):
 
     return uniqueWords
 
+def initCentroids(X, K):
+    centroidIdxs = np.random.choice(X.shape[0], K, replace=False)
+    centroids = copy.deepcopy(X[centroidIdxs])
+    return centroids
+
+def findClosestCentroids(X, centroids):
+    reviewCount = X.shape[0]
+    centroidNum = centroids.shape[0]
+    closestCentroidVector = np.zeros(reviewCount, dtype=np.int8)
+    for i in range(reviewCount):
+        dupedReview = np.repeat(X[i], centroidNum, 0)
+        bitwiseDistance = np.square(dupedReview - centroids).sum(axis=1)
+        idxOfClosest = np.argmin(bitwiseDistance)
+        closestCentroidVector[i] = idxOfClosest
+    return closestCentroidVector
+
+def computeNewCentroids(X, membership, K):
+    m, n = X.shape
+
+    arrMembership = membership.reshape(-1,1)
+    intComparator = np.arange(5).reshape(1,-1)
+    logicalFilter = np.equal(arrMembership, intComparator)
+
+    vectorSum = np.transpose(logicalFilter) * X
+    membershipTotal = logicalFilter.sum(axis=0).reshape(-1,1)
+    newCentroids = np.divide(vectorSum, membershipTotal)
+    return newCentroids
+
+def runKMeans(X, K, maxIterations):
+    m, n = X.shape
+
+    centroids = initCentroids(X, K)
+    prevCentroids = centroids
+    centroidMembership = np.zeros(m, dtype=np.int8)
+
+    for i in range(maxIterations):
+        centroidMembership = findClosestCentroids(X, centroids)
+        centroids = computeNewCentroids(X, centroidMembership, K)
+    
+    return (centroidMembership, centroids)
+
+
 cafeArr = None
 with open('trainingSet.txt') as json_file:
     cafeArr = json.load(json_file)
 
-processCafeArray(cafeArr)
+X = processCafeArray(cafeArr)
+centroidMembership, centroids = runKMeans(X, 5, 10)
 
 lol = 'hey'
 
