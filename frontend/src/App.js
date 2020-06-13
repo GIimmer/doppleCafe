36896e6cca13c6f6191801feb03e3c5994923576
Riley <rwaugh52@gmail.com>
2020-06-13 02:53:34 -0700

    Beginning integration of Auth0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,15 @@ import theme from './styles/muiTheme'
 import NavBar from './components/NavBar'
 import AppBody from './components/AppBody'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { useAuth0 } from "./react-auth0-spa";
 
 function App() {
+  const lol = useAuth0();
+
+  if (lol.loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
+import { useAuth0 } from "../react-auth0-spa";
 import { AppBar, Tabs, Tab, Button } from '@material-ui/core'
 import '../styles/styles.scss'
 import CONSTS from "../constants/Constants"
@@ -7,50 +8,38 @@ import { Link } from 'react-router-dom'
 import { withRouter } from "react-router";
 import { tabSwitchedFunc } from "../actions/stateActions";
 
-function mapStateToProps(state) {
+export const NavBar = (props) => {
+    const tabValue = [`/${CONSTS.QUERY_VIEW}`, `/${CONSTS.QUERY_OUTCOME_VIEW}`].includes(props.location) ? 0 : 1;
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+    return (
+        <div id="NavBar">
+            <AppBar position="static" className="AppBar">
+                <Tabs value={tabValue} className="tabs" aria-label="Navbar tabs">
+                    <Tab label="Find a Matching Cafe" component={Link} to={`/${CONSTS.QUERY_VIEW}`} />
+                    <Tab label="View Cafes by City" component={Link} to={`/${CONSTS.EXPLORE_VIEW}`} />
+                </Tabs>
+                {
+                    !isAuthenticated ?
+                    <Button color="inherit" onClick={() => loginWithRedirect({})} className={'loginButton'}>Login</Button>
+                    :
+                    <Button color="inherit" className={'loginButton'}>Logout</Button>
+                }
+            </AppBar>
+        </div>
+    )
+}
+
+const mapStateToProps = (state, props) => {
     return {
+        location: props.location.pathname
     }
 }
 
-function mapDispatchToProps(dispatch) {
-    return {
+const mapDispatchToProps = (dispatch) => ({
+    return: {
         tabSwitched: tabSwitchedFunc(dispatch)
     }
-}
+})
 
-export class NavBar extends Component {
-    constructor() {
-        super();
-        this.state = {
-            QueryState: {}
-        }
-    }
-
-    componentDidUpdate(prevProps) {
-        if (this.props.location !== prevProps.location) {
-            let location = this.props.location.pathname.substr(1);
-            if ([CONSTS.QUERY_VIEW, CONSTS.EXPLORE_VIEW].includes(location)) {
-                this.props.tabSwitched();
-            }
-        }
-      }
-
-    render() {
-        let tabValue = [`/${CONSTS.QUERY_VIEW}`, `/${CONSTS.QUERY_OUTCOME_VIEW}`].includes(this.props.location.pathname) ? 0 : 1;
-        return (
-            <div id="NavBar">
-                <AppBar position="static" className="AppBar">
-                    <Tabs value={tabValue} className="tabs" aria-label="Navbar tabs">
-                        <Tab label="Find a Matching Cafe" component={Link} to={`/${CONSTS.QUERY_VIEW}`} />
-                        <Tab label="View Cafes by City" component={Link} to={`/${CONSTS.EXPLORE_VIEW}`} />
-                    </Tabs>
-                    <Button color="inherit" className={'loginButton'}>Login</Button>
-                </AppBar>
-            </div>
-        )
-    }
-}
-
-const NavBarWithRouter = withRouter(connect(mapStateToProps, mapDispatchToProps)(NavBar))
-
-export default NavBarWithRouter
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NavBar))
